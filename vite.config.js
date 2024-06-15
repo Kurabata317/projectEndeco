92df366420423398a1331447dbd9647781b5f397
Kurabata317 <acayotalini@gmail.com>
2024-06-16 04:55:36 +0900

    dfsafsda

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,23 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import VueDevTools from 'vite-plugin-vue-devtools'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), VueDevTools()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    VueDevTools(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: '404.html',
+          dest: ''
+        }
+      ]
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
